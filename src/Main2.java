ed092203294257bfadffc0cccba7a91857cdde3a
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2020-11-09 09:24:28 -0800

    add second data line to plot

--- src/Main2.java
+++ src/Main2.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 
 public class Main2 extends PApplet {
-    Plot plt, plt2;         // TODO: add ability to be fixed size; new points bump off old points
+    Plot plt;         // TODO: add ability to be fixed size; new points bump off old points
     int time = 0;
 
     public void settings() {
@@ -10,26 +10,21 @@ public class Main2 extends PApplet {
 
     public void setup() {
         plt = new Plot(0, 0, 200, 200);
-        plt2 = new Plot(0, 300, 200, 200);
 
         plt.set(Plot.Setting.show_axes, true);      // TODO: make a nice api for this
         plt.set(Plot.Setting.show_border, true);
-
-        plt2.set(Plot.Setting.show_axes, true);
-        plt2.set(Plot.Setting.show_border, true);
     }
 
     public void draw() {
         background(255);
 
         if (mousePressed) {
-            plt.plot(time, mouseX).strokeColor("red");
-            plt2.plot(time, mouseY).strokeColor("green");
+            plt.plot(0, time, mouseX).strokeColor("red").style("-");
+            plt.plot(1, time, mouseY).strokeColor("green").style("-");
             time++;
         }
 
         plt.draw(this);
-        plt2.draw(this);
     }
 
     public static void main(String[] args) {
