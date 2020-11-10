b48465c2b3c65b43fdfaac664ecd123974d72ece
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2020-11-10 13:13:19 -0800

    create TimeSeriesPlot subclass of Plot

---
+++ src/TimeSeriesPlot.java
@@ -0,0 +1,12 @@
+public class TimeSeriesPlot extends Plot {
+
+    public TimeSeriesPlot(int cornerX, int cornerY, int w, int h) {
+        super(cornerX, cornerY, w, h);
+    }
+
+    @Override
+    public PlotData plot(int index, double x, double y) {
+        return null;
+    }
+
+}
