402da10b932d5550d35bcb02ae25d17a9d012d76
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2020-11-10 17:31:48 -0800

    implement shifting x window for TimeSeriesPlot

--- src/TimeSeriesPlot.java
+++ src/TimeSeriesPlot.java
@@ -1,12 +1,48 @@
 public class TimeSeriesPlot extends Plot {
+    private int dataWidth;
 
-    public TimeSeriesPlot(int cornerX, int cornerY, int w, int h) {
+    public TimeSeriesPlot(int cornerX, int cornerY, int w, int h, int width) {
         super(cornerX, cornerY, w, h);
+        this.dataWidth = width;
     }
 
     @Override
     public PlotData plot(int index, double x, double y) {
-        return null;
+        PlotData data;
+
+        if (index >= 0 && index < datasets.size()) {
+            data = datasets.get(index);
+        } else {
+            data = new PlotData();
+            this.datasets.add(data);
+        }
+
+        // If frozen scale, don't add data that's out of the range
+        if (settings.containsKey(Setting.freeze_y_scale)) {
+            if (! inDataYRange(y) ) {
+                return data;
+            }
+        }
+
+        data.add(x, y);
+
+        if (data.size() >= this.dataWidth) {
+            data.remove(0);
+            shiftXBounds(data);
+        }
+
+        if (!settings.containsKey(Setting.freeze_y_scale)) {
+            updateYBoundsWith(data);
+        }
+
+        return data;
     }
 
+    private void shiftXBounds(PlotData data) {
+        this.dataMinX = data.getDataX(0);
+        this.dataMaxX = data.getDataX(data.size()-1);
+
+        data.setDataMinX(dataMinX);
+        data.setDataMaxX(dataMaxX);
+    }
 }
