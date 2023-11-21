3d42f01cfa258c14cbd79faf7e531a9a510d250f
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-11-21 14:49:47 -0800

    Add MathUtils toList overload, and add unit tests.

--- src/Plot/MathUtils.java
+++ src/Plot/MathUtils.java
@@ -3,9 +3,9 @@ package Plot;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 /***
  * Static helper methods for doing math-related things
@@ -14,15 +14,16 @@ public class MathUtils {
 
     /***
      * Return a double[] of num values from min to max.
+     * 
      * @param min min value to start at
      * @param max max value to end at
      * @param num number of values in the array
-     * @return  a double[] of num values from min to max.
+     * @return a double[] of num values from min to max.
      */
     public static double[] linspace(double min, double max, int num) {
         double[] out = new double[num];
 
-        double dx = (max-min)/num;
+        double dx = (max - min) / num;
         for (int i = 0; i < out.length; i++) {
             out[i] = min;
             min += dx;
@@ -32,13 +33,15 @@ public class MathUtils {
     }
 
     /***
-     * Apply a function to each element in an input array.  Returns output array of return values.
+     * Apply a function to each element in an input array. Returns output array of
+     * return values.
      * For example:
      * double[] x = MathUtils.linspace( -6, 6, 500 );
      * double[] y = MathUtils.apply( Math::sin, x );
      *
      * double[] x2 = MathUtils.linspace( 1, 100, 100);
      * double[] squares = MathUtils.apply( (v -> v*v), x );
+     * 
      * @param f function to apply to each element of x
      * @param x list of inputs
      * @return array of length x.length where element i is f(x[i])
@@ -49,6 +52,7 @@ public class MathUtils {
 
     /***
      * Convert a double[] to List<Double>
+     * 
      * @param arr input array
      * @return list<Double> containing same values as arr
      */
@@ -56,14 +60,19 @@ public class MathUtils {
         return DoubleStream.of(arr).boxed().collect(Collectors.toList());
     }
 
+    public static List<Double> toList(int[] arr) {
+        return Arrays.stream(arr).mapToDouble(i -> i).boxed().collect(Collectors.toList());
+    }
+
+    public static List<Double> toList(float[] arr) {
+        return IntStream.range(0, arr.length).mapToDouble(i -> arr[i]).boxed().collect(Collectors.toList());
+    }
+
     public static double ceilToNearest(double val, double M) {
-        return Math.ceil(val/M)*M;
+        return Math.ceil(val / M) * M;
     }
 
     public static double roundToNearest(double val, double M) {
-        return Math.round(val/M)*M;
+        return Math.round(val / M) * M;
     }
-
-    // TODO: make overloaded version of toList for int[]
-    // TODO: make overloaded version of toList for float[]  (because processing uses float)
 }
